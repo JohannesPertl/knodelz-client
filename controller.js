6329992b9fe1dd5603b27903e28ba623730ce0e3
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2020-06-20 21:24:11 +0200

    implement onclick function

---
+++ controller.js
@@ -0,0 +1,18 @@
+
+function feedKnodel(){
+    const buttonFeed = document.getElementById('feedButton');
+    buttonFeed.addEventListener('click', function(e) {
+        console.log('Feed Knodus');
+    })
+
+}
+
+
+function brushKnodel(){
+    const buttonFeed = document.getElementById('brushButton');
+    buttonFeed.addEventListener('click', function(e) {
+        console.log('Brush Knodus');
+    })
+
+}
+
