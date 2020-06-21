d891a6e585d0cda37ce73243910fa5ae00879a8c
Liliana Olczak <liliana.olczak@edu.fh-joanneum.at>
2020-06-21 14:29:54 +0200

    Cleanup Refresh page if connection is lost Show foodValue and brushValue in game.html

--- client.js
+++ client.js
@@ -5,14 +5,7 @@
  */
 
 $(function () {
-    // for better performance - to avoid searching in DOM
-    var content = $("#content");
-    var input = $("#input");
-    var status = $("#status");
 
-    var token = ""
-
-    // REST
     $(document).on("click", "#createButton", function () {
         var username = $("#username");
         var password = $("#password");
@@ -38,7 +31,8 @@ $(function () {
                     headers: {},
                     success: function (data) {
                         console.log("Successful");
-                        token = data.token
+                        localStorage.setItem('token', data.token)
+                        location.replace("game.html")
                     },
                     error: function () {
                         alert("Nono!");
@@ -62,7 +56,7 @@ $(function () {
                 headers: {},
                 success: function (data) {
                     console.log("Successful");
-                    token = data.token
+                    location.replace("game.html")
                 },
                 error: function () {
                     alert("Nono!");
@@ -71,32 +65,9 @@ $(function () {
 
         });
 
-
-        // $.post(
-        //     "http://localhost:8080/api/user",
-        //     {name: "joe", password: "test123"},
-        //     success: function (data, status, xhr) {
-        //         alert("test")
-        //     });
-
-        // $.get(url, function (data) {
-        //
-        //     console.log( "Read latest ID ");
-        //     console.log(data);
-        //
-        // });
-
-    });
-
-
-
-
-
+   });
 
     // Websocket
-    // if user is running mozilla then use it's built-in WebSocket
-    window.WebSocket = window.WebSocket
-
     // if browser doesn't support WebSocket, just show some notification and exit
     if (!window.WebSocket) {
         content.html($("<p>", {text: "Sorry, but your browser doesn't support WebSocket."}));
@@ -107,30 +78,17 @@ $(function () {
 
     // TODO don't forget to use secure connection with e.g. ssl
     // open connection
-    const connection = new WebSocket("ws://127.0.0.1:8080/ws");
+    var connection = new WebSocket("ws://127.0.0.1:8080/ws");
 
 
     // WebSocket Event open and EventHandler onOpen
     connection.onopen = function () {
-
-        // // TODO: do something once connection has been established
-        // var msg = {
-        //   type: "authenticate",
-        //   payload: {
-        //     token:
-        //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMWM3NTljMC1hOTk1LTExZWEtYTVmYS05MzBjMWUxMjU0ZDAiLCJpYXQiOjE1OTIzMjYwNTB9.pcq62VlD1i4MceeSXL4WaHHgwWPgPcmVGGVzrwcnaGg",
-        //     intention: "brush",
-        //   },
-        // };
-
-
-
-        connection.send(JSON. stringify(msg));
+        console.log("Connection to server established")
     };
 
 
     $(document).on("click", "#feedButton", function () {
-        alert("feed")
+        token = localStorage.getItem("token")
         var msg = {
             type: "authenticate",
             payload: {
@@ -143,12 +101,11 @@ $(function () {
     })
 
     $(document).on("click", "#brushButton", function () {
-        alert("brush")
+        token = localStorage.getItem("token")
         var msg = {
             type: "authenticate",
             payload: {
-                token:
-                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMWM3NTljMC1hOTk1LTExZWEtYTVmYS05MzBjMWUxMjU0ZDAiLCJpYXQiOjE1OTIzMjYwNTB9.pcq62VlD1i4MceeSXL4WaHHgwWPgPcmVGGVzrwcnaGg",
+                token: token,
                 intention: "brush",
             },
         };
@@ -169,86 +126,49 @@ $(function () {
     };
 
     // WebSocket Event message and EventHandler onMessage
-    // most important part - incoming messages
     connection.onmessage = function (message) {
         console.log("onmessage");
-        // try to parse JSON message. Because we know that the server
-        // always returns JSON this should work without any problem but
-        // we should make sure that the massage is not chunked or
-        // otherwise damaged.
+
         try {
             var json = JSON.parse(message.data);
+
+            $("#foodValue").html(json.food)
+            $("#brushValue").html(json.brush)
+
+            console.log(json)
+
+            if (json.dead) {
+                token = localStorage.getItem("token")
+
+                var msg = {
+                    type: "authenticate",
+                    payload: {
+                        token: token,
+                        intention: "reset",
+                    },
+                };
+                connection.send(JSON.stringify(msg))
+
+
+                location.replace("dead.html")
+            }
         } catch (e) {
-            console.log("Invalid JSON: ", message.data);
-            return;
+            console.log(e);
         }
 
-        // NOTE: if you're not sure about the JSON structure
-        // check the server source code above
-        // first response from the server with user's color
-        if (json.type === "message") {
-            // it's a single message
-            // let the user write another message
-            input.removeAttr("disabled");
-            addMessage(json.data.text, new Date(json.data.time));
-        } else {
-            console.log("Hmm..., I've never seen JSON like this:", json);
-        }
     };
 
 
-    // /**
-    //  * Send message when user presses Enter key
-    //  */
-    // input.keydown(function (e) {
-    //   if (e.keyCode === 13) {
-    //     var msg = $(this).val();
-    //     if (!msg) {
-    //       return;
-    //     }
-    //
-    //     // extended message object, including bearer token, to verify that message source is authenticated before
-    //     var obj = {
-    //       text: msg,
-    //       token: bearerToken,
-    //     };
-    //
-    //     var json = JSON.stringify(obj);
-    //     // send the message as an ordinary text
-    //     connection.send(json);
-    //     $(this).val("");
-    //     // disable the input field to make the user wait until server
-    //     // sends back response
-    //     input.attr("disabled", "disabled");
-    //   }
-    // });
-
     /**
-     * This method is optional. If the server wasn't able to
-     * respond to the request in 3 seconds then show some error message
-     * to notify the user that something is wrong.
+     * If the server wasn't able to
+     * respond to the request in 10 seconds,
+     * then refresh the page to reset connection
      */
     setInterval(function () {
         if (connection.readyState !== 1) {
-            status.text("Error");
-            input.attr("disabled", "disabled").val("Unable to communicate with the WebSocket server.");
+            console.log("Server is down, refreshing page..");
+            location.reload()
         }
-    }, 3000);
-
-    // /**
-    //  * Add message to the chat window
-    //  */
-    // function addMessage(message, dt) {
-    //   console.log(message);
-    //   content.prepend(
-    //     '<p class="chat-message">' +
-    //       (dt.getHours() < 10 ? "0" + dt.getHours() : dt.getHours()) +
-    //       ":" +
-    //       (dt.getMinutes() < 10 ? "0" + dt.getMinutes() : dt.getMinutes()) +
-    //       ": " +
-    //       message +
-    //       "</p>"
-    //   );
-    // }\
+    }, 10000);
 
 });
